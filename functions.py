f60f5f94172f9ff8a0e3ad6a0b5e782c0d9b006d
Pixel <61188160+Rtrust@users.noreply.github.com>
2023-02-02 14:56:30 +0100

    exo3

--- functions.py
+++ functions.py
@@ -1,12 +1,20 @@
-# exo1
-i = 1
-while i < 6:
-    print("*"*i)
-    i= i + 1
+# # exo1
+# i = 1
+# while i < 6:
+#     print("*"*i)
+#     i= i + 1
 
-#exo2
-for i in range(1,6):
-    if i%2 == 0:
-        print('')
-    else:
-        print(i*'*')
+# #exo2
+# for i in range(1,6):
+#     if i%2 == 0:
+#         print('')
+#     else:
+#         print(i*'*')
+
+#exo3
+todo = ["exercise1", "exercise2", "exercise3","coffee break", "exercise4","exercise5","exercise6"]
+for x in todo:
+    if x.lower == "COFFEE BREAK":
+        break
+    print(x)
+        
