55f5b5380f918e279e4ca1ecad4d803228f197a7
Pixel <61188160+Rtrust@users.noreply.github.com>
2023-02-04 17:08:12 +0100

    exo4

--- functions.py
+++ functions.py
@@ -11,10 +11,23 @@
 #     else:
 #         print(i*'*')
 
-#exo3
-todo = ["exercise1", "exercise2", "exercise3","coffee break", "exercise4","exercise5","exercise6"]
-for x in todo:
-    if x.lower == "COFFEE BREAK":
-        break
-    print(x)
+# #exo3
+# todo = ["exercise1", "exercise2", "exercise3","coffee break", "exercise4","exercise5","exercise6"]
+# for x in todo:
+#     if x.lower == "COFFEE BREAK":
+#         break
+#     print(x)
         
+# #exo4
+list1 = ["lion", "monkey", "dog","fish"]
+tuple1 = ("lion", "monkey", "dog","fish")
+set1 = {"lion", "monkey", "dog","fish"}
+dict1 = {"lion":"land", "monkey":"land", "dog":"land","fish":"water"}
+
+for i in list1, tuple, set1:
+    print(i)
+for i, j in dict1.items():
+    if j == 'land':
+        print(i)
+
+
