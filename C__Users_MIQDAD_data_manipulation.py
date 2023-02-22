953b0e11821cf947844d101ba0288e908870a60b
GHAZAL <you@example.com>
2023-02-22 11:12:53 +0300

    data manipulation 1

---
+++ C__Users_MIQDAD_data_manipulation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+pandas.__version__
+
+
+# In[13]:
+
+
+dataset_1 = pd.read_csv(r"C:\Users\MIQDAD\data\googleplaystore.csv")
+
+
+# In[14]:
+
+
+print(dataset_1)
+
+
+# In[15]:
+
+
+dataset_2 = pd.read_csv(r"C:\Users\MIQDAD\data\googleplaystore_user_reviews.csv")
+
+
+# In[16]:
+
+
+print(dataset_2)
+
+
+# In[ ]:
+
+
+
+
