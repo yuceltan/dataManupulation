3d71713ffd4e0496b916733fab3f4f8bf8bc4312
Pixel <61188160+Rtrust@users.noreply.github.com>
2023-02-25 19:57:54 +0100

    correction

---
+++ exo.py
@@ -0,0 +1,32 @@
+import pandas as pd
+# Load the dataset
+df = pd.read_csv('/Users/pc home/Downloads/googleplaystore.csv')
+
+# Drop duplicates
+df.drop_duplicates(subset='App', inplace=True)
+
+# Drop rows with missing values
+df.dropna(inplace=True)
+
+# Convert 'Reviews', 'Size', and 'Installs' columns to numerical values
+df['Reviews'] = df['Reviews'].astype(int)
+# df['Size'] = df['Size'].apply(lambda x: float(x.strip('M').replace(',', '')) if 'M' in x else float(x.strip('k').replace(',', '')) / 1000)
+df['Installs'] = df['Installs'].apply(lambda x: int(x.strip('+').replace(',', '')))
+
+# Clean 'Type' column
+df['Type'] = df['Type'].apply(lambda x: 1 if x == 'Paid' else 0)
+
+# Clean 'Price' column
+df['Price'] = df['Price'].apply(lambda x: float(x.strip('$')) if x != '0' else 0)
+
+# Clean 'Content Rating' column
+df['Content Rating'] = df['Content Rating'].apply(lambda x: 'Everyone' if x == 'Unrated' else x)
+
+# Convert 'Genres' column to a list of genres
+df['Genres'] = df['Genres'].apply(lambda x: x.split(';')[0])
+
+# Reset index
+df.reset_index(drop=True, inplace=True)
+
+# Save cleaned dataset to a new CSV file
+df.to_csv('cleaned_googleplaystore1.csv', index=False)
